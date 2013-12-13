2d6801a3e376dedd428f041daba30bd09582945c
Pblolpb <Pblolpb@hotmail.ca>
2013-12-13 01:45:36 -0500

    Fixed an issue

--- src/com/github/pblolpb/noshift/NoShift.java
+++ src/com/github/pblolpb/noshift/NoShift.java
@@ -1,12 +1,20 @@
 package com.github.pblolpb.noshift;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoShift extends JavaPlugin implements Listener {
 
+	public void onEnable () {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
+	}
+
+	@EventHandler
 	public void onPlayerShift(PlayerToggleSneakEvent event){
 		Player player = event.getPlayer();
 		if(!player.hasPermission("noshift.shift")){
